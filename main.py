e181a6b24e71f66b05b19eca4eaa52c0fe0ed433
Korin <kav02@outlook.com>
2024-03-20 10:33:16 -0600

    Agregar las mejoras del computador para adivinar el número

--- main.py
+++ main.py
@@ -4,38 +4,54 @@
 # pylint: disable=C0103
 import random
 #Generar un número aleatorio entre 1 y 100
-secret_number= random.randint(1,100)
-print(secret_number)
+#secret_number= random.randint(1,100)
+#print(secret_number)
 #Ingresar el nombre del participante
 participant_name = input("Por favor ingresar su nombre?  ")
 participants = [participant_name,"Computadora"]
-player_guess=[]
+participant_guess=[]
 computer_guess=[]
 #Variables globales usadas para ajustar los intentos de la computadora
 low_range = 1
 high_range = 100
+#Inicializar el juego
+def reset_game():
+    global secret_number, participant_guess, computer_guess, attempts, low_range, high_range
+    secret_number = random.randint(1, 100)
+    participant_guess = []
+    computer_guess = []
+    attempts = 0
+    low_range = 1
+    high_range = 100
+
+#Resetear el juego
+reset_game()
+
 #Turnos
 def turn(player):
     #Solicitar un número usando un loop para guardar cada intento
-    if player == participant_name:
-        guess = int(input("Ingresa un número del 1 al 100: "))
-        if guess > 100 or guess < 0:
-            print('Ingreso inválido, ingresa un número del 1 al 100')
+    while True:
+        if player == participant_name:
+            guess = int(input("Ingresa un número del 1 al 100: "))
+            if 1 <= guess <= 100:
+                participant_guess.append(guess)
+                break
+            else:
+                print('El número ingresado no es válido, debe ser un número del 1 al 100')
         else:
-            player_guess.append(guess)
-    else:
-        guess = round((low_range + high_range)/2)
-        print("Intento de la computadora: "+ str(guess))
-        computer_guess.append(guess)
+            guess = round((low_range + high_range)/2)
+            print("Intento de la computadora: "+ str(guess))
+            computer_guess.append(guess)
+            break
     return player, guess
 
 #Evaluar el intento
 def number_evaluation(player, guess):
     global low_range, high_range
     if guess == secret_number:
-        print('El ganador es', player + '! El número secreto es:', str(secret_number) + ' y lo lograste en', str(ATTEMPTS) + ' intentos')
-        print('Tus intentos fueron: ', player_guess)
-        return False
+        print('El ganador es', player + '! El número secreto es:', str(secret_number) + ' y lo ha adivinado en', str(attempts) + ' intentos')
+        print('Los intentos de ', participant_name + ' fueron: ', participant_guess)
+        return True
     if guess > secret_number:
         print("El número es muy alto")
         if guess < high_range:
@@ -44,17 +60,21 @@ def number_evaluation(player, guess):
         print("El número es muy bajo")
         if guess > low_range:
             low_range = guess
-    return True
+    return False
 
 # Compararlo con el número secreto
-GUESSING = True
-ATTEMPTS = 0
-while GUESSING:
-    ATTEMPTS += 1
+guessing = True
+attempts = 0
+while guessing:
+    attempts += 1
     for participant in participants:
         playing = turn(participant)
         RESULT = number_evaluation(playing[0], playing[1])
-        if RESULT is False:
-            GUESSING = False
-            break
-        
+        if RESULT is True:
+            play_again = input("¿Quieres jugar de nuevo? S/N ")
+            if play_again.upper() == "S":
+                reset_game()
+            else:
+                guessing = False
+                print("¡Gracias por participar!")
+                break
--- tests/test_function.py
+++ tests/test_function.py
@@ -1,1 +1,15 @@
-import unittest
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=C0103
+
+from main import turn
+def test_turn_invalid_input(self):
+    participant_name = "Ana"
+    guess = 150   
+    turn(participant_name) 
+    self.assertFalse(guess,150)
+    
+def test_number_evaluation():
+    secret_number = 92
+    
